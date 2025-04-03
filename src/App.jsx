e55a42652189b3d3fa2f2644a1d81ed5523742a1
lucia <malunesi17@gmail.com>
2025-04-02 22:16:16 -0300

    botao linkedin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Gi3dGlasses } from "react-icons/gi";
 import './App.css'
+import Linkedin from "./components/Linkedin";
 
 function App() {
   
@@ -11,6 +12,9 @@ function App() {
       <a href='https://github.com/MARIALUCIA2024' className='links' target='_blank'>
       <Gi3dGlasses />Github
       </a>
+
+      <Linkedin/>
+      
       
     </div>
   )
---
+++ src/components/Linkedin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FaLinkedin } from "react-icons/fa";
+
+function Linkedin() {
+  return (
+
+    <div className='container-linkedin links'>
+        <FaLinkedin />
+        Linkedin
+        </div>
+  )
+}
+
+export default Linkedin
