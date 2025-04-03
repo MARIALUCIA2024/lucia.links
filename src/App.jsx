499fb0cfb968094f05f78d1f035f53cd8264602f
lucia <malunesi17@gmail.com>
2025-04-02 21:16:06 -0300

    alteracao

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Gi3dGlasses } from "react-icons/gi";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+    <div className='container-app'>
+      <h1> links do meu S2</h1>
+      <a href='https://github.com/MARIALUCIA2024' className='links' target='_blank'>
+      <Gi3dGlasses />Github
+      </a>
+      
+    </div>
+  )
+}
+
+export default App
